1d2f30f11038d26b0f288e47a56cea6b55025a56
dev_21 <o.savchenko@ukrspecsystems.com>
2021-08-04 18:35:05 +0300

    add new button

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+"""
+Author:Savchenko Oleksii
+Data: 02.2021
+
+
+"""
+
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog, Text, Label, Toplevel
+from time import strftime
 import os
 
 
 root = tk.Tk()
+root.title('Тренажер БПЛА (beta version)')
+root.iconphoto(False, tk.PhotoImage(file='sim.png'))
 apps = []
 
 if os.path.isfile('save.txt'):
@@ -12,6 +22,22 @@ if os.path.isfile('save.txt'):
         tempApps = tempApps.split(',')
         apps = [x for x in tempApps if x.strip()]
         print(apps)
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
+
+
+lbl = Label(root, font=('calibri', 14, 'bold'),
+            background='black',
+            foreground='white')
+
+# Placing clock at the centre
+# of the tkinter window
+lbl.pack(anchor='e')
+time()
 
 
 def addApp():
@@ -28,24 +54,53 @@ def addApp():
         label.pack()
 
 
+def open_win():
+   global top
+   top = Toplevel(root)
+   Label(top, text= "Manual for operator", font= ('Helvetica 14 bold')).pack()
+   canvas = tk.Canvas(top, height=520, width=480, bg="#263D42", cursor='crosshair')
+   canvas.pack(expand=False)
+
+   frame = tk.Frame(top, bg="white")
+   frame.place(relx=0.06, rely=0.10, relwidth=0.9, relheight=0.6)
+
+   closeWin = tk.Button(top, text="Close", padx=35, fg='white', bg="#263D42", command=close_newapps)
+   closeWin.pack()
+   top.grab_set()
+
+
 def runApps():
     for app in apps:
         os.startfile(app)
 
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
-canvas.pack()
+def close_apps():
+    print("Window closed by customer")
+    root.destroy()
+
+
+def close_newapps():
+    print("PopWin closed by customer")
+    top.destroy()
+
+canvas = tk.Canvas(root, height=520, width=480, bg="#263D42", cursor='crosshair')
+canvas.pack(expand=False)
 
 frame = tk.Frame(root, bg="white")
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+frame.place(relx=0.06, rely=0.10, relwidth=0.9, relheight=0.6)
 
-openFile = tk.Button(root, text="Open File", padx=25,
-                     pady=10, fg='white', bg="#263D42", command=addApp)
+openFile = tk.Button(root, text="Open File", padx=25, fg='white', bg="#263D42", command=addApp)
 openFile.pack()
 
-runApps = tk.Button(root, text="Run Apps", padx=25,
-                    pady=10, fg='white', bg="#263D42", command=runApps)
+runApps = tk.Button(root, text="Run Apps", padx=25, fg='white', bg="#263D42", command=runApps)
 runApps.pack()
+
+open_window = tk.Button(root, text="New window", padx=35, fg='white', bg="#263D42", command=open_win)
+open_window.pack()
+
+closeWin = tk.Button(root, text="Close", padx=35, fg='white', bg="#263D42", command=close_apps)
+closeWin.pack()
+
 
 for app in apps:
     label = tk.Label(frame, text=app)
